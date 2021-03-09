57b7af2eebc7ed5b936d5017007cb6f6c1a33509
onknows <onno@onknows.com>
2021-03-09 14:16:38 +0100

    tomcat role more robust - in dev fqdn can become hostname via ajsalminen.hosts and common_manage_hosts: true

--- plugins/filter/tomcat_filters.py
+++ plugins/filter/tomcat_filters.py
@@ -3,16 +3,19 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from ansible.errors import AnsibleFilterError
-import json, os
 
 # Return path to properties folder
 # e.g. /etc/tomcat/myapp.properties
-def tomcat_properties_file_path(app,properties_folder):
+def tomcat_properties_file_path(app, properties_folder):
     return '/'.join([properties_folder, app + '.properties'])
 
+
 def tomcat_domain(ansible_fqdn):
-    return ansible_fqdn.split('.', 1)[1]
+    if '.' in ansible_fqdn:
+        return ansible_fqdn.split('.', 1)[1]
+    else:
+        return 'local'
+
 
 class FilterModule(object):
     """backup filters."""
